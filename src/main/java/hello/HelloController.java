d236c8b368f954e5a606c1e1c566ac61ece0cc91
Muhammad Awab <98603853+Muhammad-Awab@users.noreply.github.com>
2023-06-22 22:58:40 +0500

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
         public String index() {
-            return "This is the index awab ahmed!\n";
+            return "This is the index awab ahmed noveenn !\n";
         }
     @RequestMapping("/hello")
         public String index2() {
